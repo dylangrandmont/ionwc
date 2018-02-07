eda7622d49c10764175f4ffd3badff57644ad27c
Dylan <dylan@dylan-VPCEH13FD.(none)>
2018-02-06 20:08:33 -0700

    update to pep8 standard

--- facilities.py
+++ facilities.py
@@ -12,56 +12,54 @@ from constants import UNKNOWN
 
 IONWC_HOME = os.environ["IONWC_HOME"]
 
-def conformUWI(uwi):
-  if "/" in uwi:
-    grid = "NTS"
-  else:
-    grid = "DLS"
+def conform_uwi(uwi):
+    if "/" in uwi:
+        grid = "NTS"
+    else:
+        grid = "DLS"
 
-  if grid=="NTS":
-    uwi = "200" + uwi.replace("/", "").replace("-", "") + "00"
-  else:
-    uwi = uwi.split("-")
-    uwi = "00/" + uwi[0] + '-' + uwi[1] + '-' + uwi[2] + "W6/0"
+    if grid=="NTS":
+        uwi = "200" + uwi.replace("/", "").replace("-", "") + "00"
+    else:
+        uwi = uwi.split("-")
+        uwi = "00/" + uwi[0] + '-' + uwi[1] + '-' + uwi[2] + "W6/0"
 
-  return uwi
+    return uwi
 
 def addBCFacilitiesToDataBase(facilitiesFileAll):
-  """ Query for all BC facilities, parse them into a common format and add to database (csv) """
-  bcfacilties = 0
+    """ Query for all BC facilities, parse them into a common format and add to database (csv) """
+    bcfacilties = 0
 
-  allBCLics = open(IONWC_HOME + "/data/facilities/bc/facilities.csv")
-  reader = csv.reader(allBCLics)
+    allBCLics = open(IONWC_HOME + "/data/facilities/bc/facilities.csv")
+    reader = csv.reader(allBCLics)
 
-  # Skip header (first row)
-  next(reader)
+    # Skip header (first row)
+    next(reader)
 
-  for row in reader:
-    facilityId = row[1]
-    facilityType = row[2]
-    objectType = row[3]
-    activity = row[4]
-    organization = row[5]
-    location = row[6]     # B-023-E/094-A-14 --> 200B077D094H0300  or 06-06-086-13
-    approvalDate = row[7]
-    constructionStartDate = row[8]
-    pressureTestDate = row[9]
-    leaveToOpenDate = row[10]
-    asBuiltDate = row[11]
-    tenureFileNumber = row[12]
-    legalSurveyApprovalDate = row[13]
-    facilityStatus = row[14]
+    for row in reader:
+        facilityId = row[1]
+        facilityType = row[2]
+        objectType = row[3]
+        activity = row[4]
+        organization = row[5]
+        location = row[6]     # B-023-E/094-A-14 --> 200B077D094H0300  or 06-06-086-13
+        approvalDate = row[7]
+        constructionStartDate = row[8]
+        pressureTestDate = row[9]
+        leaveToOpenDate = row[10]
+        asBuiltDate = row[11]
+        tenureFileNumber = row[12]
+        legalSurveyApprovalDate = row[13]
+        facilityStatus = row[14]
 
-    uwi = conformUWI(location)
-                   
-    lat, lon = uwiToLatLng.convert(uwi)                                                                          
-    random.seed(int(re.sub("[^0-9]","", facilityId)))
-    lat += random.uniform(-0.0005,0.0005)
-    lon += random.uniform(-0.0005,0.0005)
+        uwi = conform_uwi(location)
+                       
+        lat, lon = uwiToLatLng.convert(uwi)                                                                          
+        random.seed(int(re.sub("[^0-9]","", facilityId)))
+        lat += random.uniform(-0.0005,0.0005)
+        lon += random.uniform(-0.0005,0.0005)
 
-    writeFaciltiesFile(facilitiesFileAll, facilityId, facilityType, organization, lat, lng)
-    bcfacilties += 1                                                                                                 
-                                                                                      
-  print "INFO:  Found", bcfacilties, " facilities for BC"   
-
-"""
+        writeFaciltiesFile(facilitiesFileAll, facilityId, facilityType, organization, lat, lng)
+        bcfacilties += 1                                                                                                 
+                                                                                        
+    print "INFO:  Found", bcfacilties, " facilities for BC"   
